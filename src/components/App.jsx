e8480ae4048e31fed2d3f3b7ca02e05ff31ab528
Brudja <n0669378587@gmail.com>
2022-11-09 10:20:27 +0200

    Start

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+
+export const App = () => {
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexWrap: 'nowrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+        flexDirection: 'column',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactForm/>
+      <h2>Contacts</h2>
+      <Filter/>
+      <ContactList/>
+    </div>
+  );
+}
---
+++ src/redux/PhoneBookRedux/selector.js
@@ -0,0 +1,7 @@
+export const getFilteredContacts = state => {
+    return state.phonebook.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(state.filter.filter.toLowerCase())
+    );
+  };
+  export const getFilter = state => state.filter.filter;
+  
